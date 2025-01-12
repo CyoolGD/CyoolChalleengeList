b8236306903ecc92ad30fec418e5ddc101d86c1c
Cyool <cyool.email@gmail.com>
2025-01-11 19:34:17 -0500

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,3 +1,51 @@
+import { round, score } from './score.js';
+
+/**
+ * Path to directory containing _list.json and all levels
+ */
+const dir = '/data';
+
+export async function fetchList() {
+    const listResult = await fetch(${dir}/_list.json);
+    try {
+        const list = await listResult.json();
+        return await Promise.all(
+            list.map(async (path, rank) => {
+                const levelResult = await fetch(${dir}/${path}.json);
+                try {
+                    const level = await levelResult.json();
+                    return [
+                        {
+                            ...level,
+                            path,
+                            records: level.records.sort(
+                                (a, b) => b.percent - a.percent,
+                            ),
+                        },
+                        null,
+                    ];
+                } catch {
+                    console.error(Failed to load level #${rank + 1} ${path}.);
+                    return [null, path];
+                }
+            }),
+        );
+    } catch {
+        console.error(Failed to load list.);
+        return null;
+    }
+}
+
+export async function fetchEditors() {
+    try {
+        const editorsResults = await fetch(${dir}/_editors.json);
+        const editors = await editorsResults.json();
+        return editors;
+    } catch {
+        return null;
+    }
+}
+
 export async function fetchLeaderboard() {
     const list = await fetchList();
 
@@ -10,12 +58,9 @@ export async function fetchLeaderboard() {
         }
 
         // Verification
-        const verifier = level.verifier && typeof level.verifier === 'string'
-            ? Object.keys(scoreMap).find(
-                  (u) => u.toLowerCase() === level.verifier.toLowerCase(),
-              ) || level.verifier
-            : level.verifier;
-        
+        const verifier = Object.keys(scoreMap).find(
+            (u) => u.toLowerCase() === level.verifier.toLowerCase(),
+        ) || level.verifier;
         scoreMap[verifier] ??= {
             verified: [],
             completed: [],
@@ -31,12 +76,9 @@ export async function fetchLeaderboard() {
 
         // Records
         level.records.forEach((record) => {
-            const user = record.user && typeof record.user === 'string'
-                ? Object.keys(scoreMap).find(
-                      (u) => u.toLowerCase() === record.user.toLowerCase(),
-                  ) || record.user
-                : record.user;
-            
+            const user = Object.keys(scoreMap).find(
+                (u) => u.toLowerCase() === record.user.toLowerCase(),
+            ) || record.user;
             scoreMap[user] ??= {
                 verified: [],
                 completed: [],
