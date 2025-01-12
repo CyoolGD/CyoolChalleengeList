56fa92a2b208bd302557f94b706457bcc483d129
Cyool <cyool.email@gmail.com>
2025-01-11 22:48:54 -0500

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,17 +1,17 @@
 import { round, score } from './score.js';
 
 /**
- * Path to directory containing _list.json and all levels
+ * Path to directory containing `_list.json` and all levels
  */
 const dir = '/data';
 
 export async function fetchList() {
-    const listResult = await fetch(${dir}/_list.json);
+    const listResult = await fetch(`${dir}/_list.json`);
     try {
         const list = await listResult.json();
         return await Promise.all(
             list.map(async (path, rank) => {
-                const levelResult = await fetch(${dir}/${path}.json);
+                const levelResult = await fetch(`${dir}/${path}.json`);
                 try {
                     const level = await levelResult.json();
                     return [
@@ -25,20 +25,20 @@ export async function fetchList() {
                         null,
                     ];
                 } catch {
-                    console.error(Failed to load level #${rank + 1} ${path}.);
+                    console.error(`Failed to load level #${rank + 1} ${path}.`);
                     return [null, path];
                 }
             }),
         );
     } catch {
-        console.error(Failed to load list.);
+        console.error(`Failed to load list.`);
         return null;
     }
 }
 
 export async function fetchEditors() {
     try {
-        const editorsResults = await fetch(${dir}/_editors.json);
+        const editorsResults = await fetch(`${dir}/_editors.json`);
         const editors = await editorsResults.json();
         return editors;
     } catch {
